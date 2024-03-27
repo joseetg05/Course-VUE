95754256ffca85743f5d0534fdb94891aa8764c9
joseetg05 <161364160+joseetg05@users.noreply.github.com>
2024-03-27 11:10:39 -0600

    Commit with section 7 content

---
+++ Section7-80/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+        <h2> {{ friend.name }} </h2>
+        <button @click="toggleDetails">{‌{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li> <strong>Phone:</strong> {{ friend.phone }} </li>
+            <li> <strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'Chino',
+                name: 'Chino Emilio',
+                phone: '0123 1234',
+                email: 'chino@gmail.com'
+            },
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+
+};
+</script>
