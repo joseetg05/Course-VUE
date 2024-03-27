87b801e6253b64787195a19c4c7e80d53afa4593
joseetg05 <161364160+joseetg05@users.noreply.github.com>
2024-03-27 11:18:21 -0600

    Commit with content of the section 8

---
+++ Section8-93/src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label> Name </label>
+            <input type="text" v-model="enteredName" />
+        </div>
+        <div>
+            <label> Phone Number </label>
+            <input type="tel" v-model="enteredPhone" />
+        </div>
+        <div>
+            <label> Mail Address </label>
+            <input type="email" v-model="enteredMail"/>
+        </div>
+        <div>
+            <button> Add Contact </button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['add-contact'],
+    data(){
+        return{
+            enteredName: '',
+            enteredPhone: '',
+            enteredMail: ''
+        }
+    },
+    methods:{
+        submitData(){
+            this.$emit(
+            'add-contact',
+            this.enteredName,
+            this.enteredPhone,
+            this.enteredMail
+            );
+        }
+    }
+};
+</script>
