6e4a6ee18abded323136ec863df551ff4da7e1a7
joseetg05 <161364160+joseetg05@users.noreply.github.com>
2024-03-27 11:54:04 -0600

    Commit with the content of the Summary-Section 8

---
+++ Summary-Section8-102/src/components/ActiveUser.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2> <strong>Username: </strong> {{ username }} </h2>
+        <h3> <strong>Age: </strong> {{ age }} </h3>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: {
+            id:{
+                tyoe: String,
+                required: true
+            },
+            username: {
+                type: String,
+                required: true
+            },
+            age:{
+                type: String,
+                required: true
+            }
+        }, 
+        emits: [],
+        data() {
+
+        },
+        methods: {
+
+        }
+
+    }
+</script>
---
+++ Summary-Section8-102/src/components/UserData.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <label> UserName </label>
+            <input type="text" v-model="enteredUserName">
+        </div>
+        <div>
+            <label> Age </label>
+            <input type="text" v-model="enteredAge">
+        </div>
+        <div>
+            <button> Add User</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['add-user'],
+    data() {
+        return{
+            enteredUserName: '',
+            enteredAge: '',
+        }
+    },
+    methods:{
+        submitData(){
+            this.$emit(
+            'add-user',
+            this.enteredUserName,
+            this.enteredAge
+            );
+        }
+    }
+}
+
+</script>
